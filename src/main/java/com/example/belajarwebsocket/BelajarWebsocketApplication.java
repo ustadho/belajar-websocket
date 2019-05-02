2df2d0060d7748d04093630e55d6b2f22a116adb
Ustadho_1218 <cak.ust@gmail.com>
2019-05-02 08:34:37 +0700

    first commit

---
+++ src/main/java/com/example/belajarwebsocket/BelajarWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.belajarwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/belajarwebsocket/BelajarWebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.belajarwebsocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BelajarWebsocketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
